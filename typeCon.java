db9a671bc364d42a8a4e9d41000704b77f2d5fb0
Dhruvin Patel <136917226+codewithdhruvinpatel@users.noreply.github.com>
2023-12-12 11:47:53 +0530

    Add files via upload

---
+++ typeCon.java
@@ -0,0 +1,11 @@
+class typeCon{
+public static void main(String args[])
+{
+byte b = 26;
+int i = b;
+System.out.println("Byte Value = " + b + "Ing Value = " + i);
+double d = 100.123456789d;
+int a = (int)d;
+System.out.println("Dubble Value = " + d + "Ing Value = " + a);
+}
+}

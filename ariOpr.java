3c35fb6d7ee5a209802672d1e850b891bdf93d7a
Dhruvin Patel <136917226+codewithdhruvinpatel@users.noreply.github.com>
2023-12-12 10:26:01 +0530

    Add files via upload

---
+++ ariOpr.java
@@ -0,0 +1,21 @@
+class ariOpr
+{
+public static void main(String args[])
+{
+int a=20;
+int b=10;
+int value;
+
+value = a + b;
+System.out.println("Sum =" + value);
+value = b - a;
+System.out.println("Sub =" + value);
+value = a * b;
+System.out.println("Mul =" + value);
+value = a / b;
+System.out.println("Div =" + value);
+value = a % b;
+System.out.println("Mod =" + value);
+
+}
+}

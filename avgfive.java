4f0c9b87a3deb91b46263990423030a7e4246d6d
Dhruvin Patel <136917226+codewithdhruvinpatel@users.noreply.github.com>
2023-12-12 10:40:05 +0530

    Add files via upload

---
+++ avgfive.java
@@ -0,0 +1,10 @@
+class avgfive{
+public static void main(String args[])
+{
+int a=10,b=10,c=10,d=10,e=10;;
+int sum=a+b+c+d+e;;
+int avg=sum/5;
+System.out.println("Sum =" + sum);
+System.out.println("Avg =" + avg);
+}
+}

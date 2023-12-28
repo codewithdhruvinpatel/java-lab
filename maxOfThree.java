d02656bd4986fd7a49904879f9622726991674d0
Dhruvin Patel <136917226+codewithdhruvinpatel@users.noreply.github.com>
2023-12-28 13:43:45 +0530

    Add files via upload

---
+++ maxOfThree.java
@@ -0,0 +1,15 @@
+class maxOfThree{
+public static void main(String args[])
+{
+int a=10,b=20,c=30;
+if(a>b & a>c){
+System.out.println(a + "Is Greter");
+}
+else if(b>a & b>c){
+System.out.println(b + "Is Greter");
+}
+else{
+System.out.println(c + "Is Greter");
+}
+}
+}

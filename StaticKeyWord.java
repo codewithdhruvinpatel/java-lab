d9a93e8379f66b94f987bc28b7749f7d6a2bcaf9
Dhruvin Patel <136917226+codewithdhruvinpatel@users.noreply.github.com>
2024-01-11 13:17:03 +0530

    Add files via upload

---
+++ StaticKeyWord.java
@@ -0,0 +1,11 @@
+class StaticKeyWord{
+public static void main(String args[]){
+StaticKeyWord s = new StaticKeyWord();
+}
+StaticKeyWord(){
+System.out.println("Constructor");
+}
+static{
+System.out.println("Static");
+}
+}
